3d545ad5072fc3365faee6c3cb4c303be6e6a7fa
claudiamendes <1ribev81@solent.ac.uk>
2024-05-08 22:12:22 +0100

    server updat

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ var server = express();
 var routes = require('./routes/routes');
 var mongoose = require('mongoose');
 const cors = require('cors');
-mongoose.connect("mongodb+srv://Joel:123@full-stack-web-applicat.fqa1a2h.mongodb.net/SolentNursery_childList",{useNewUrlParser: true,  useUnifiedTopology: true },function checkDB(error)
+
+
+
+
+mongoose.connect("mongodb+srv://Joel:123@full-stack-web-applicat.fqa1a2h.mongodb.net/abc",{useNewUrlParser: true,  useUnifiedTopology: true },function checkDB(error)
 {
     if(error)
     {
-        console.log("errorr")
+        console.log("error login db")
     }
     else
     {
-        console.log("Mongo DB Connected!")
+        console.log("Mongo login DB Connected !")
     }
 });
 
