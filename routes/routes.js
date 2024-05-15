d5807fed4ed7922cb2637bb171c921ac8f067190
claudiamendes <1ribev81@solent.ac.uk>
2024-05-15 21:26:08 +0100

    comments

--- routes/routes.js
+++ routes/routes.js
@@ -1,32 +1,29 @@
 // Import the 'express' module to use its functionalities for building web apps and APIs.
-var express = require('express');
+var express = require("express");
 
 // Create a router object from Express to handle routing of requests.
 const router = express.Router();
 
 // Import the user controller to handle requests related to user operations.
-var userController = require('../src/user/userController');
+var userController = require("../src/user/userController");
 
 // Import the contact controller to handle requests related to contact operations.
-var contactController = require('../src/contact/contactController');
-
-
+var contactController = require("../src/contact/contactController");
 
 // Define a route to handle GET requests for fetching all users using the user controller's function.
-router.route('/user/getAll').get(userController.getDataConntrollerfn);
+router.route("/user/getAll").get(userController.getDataConntrollerfn);
 
 // Define a route to handle POST requests for creating a new user using the user controller's function.
-router.route('/user/create').post(userController.createUserControllerFn);
+router.route("/user/create").post(userController.createUserControllerFn);
 
 // Define a route to handle PATCH requests for updating an existing user by their ID using the user controller's function.
-router.route('/user/update/:id').patch(userController.updateUserController);
+router.route("/user/update/:id").patch(userController.updateUserController);
 
 // Define a route to handle DELETE requests for deleting a user by their ID using the user controller's function.
-router.route('/user/delete/:id').delete(userController.deleteUserController);
+router.route("/user/delete/:id").delete(userController.deleteUserController);
 
 // Define a route to handle POST requests for creating a new contact using the contact controller's function.
-router.route('/contact').post(contactController.createContact);
-
+router.route("/contact").post(contactController.createContact);
 
 // Export the router object so it can be used by other parts of the application.
 module.exports = router;
--- server.js
+++ server.js
@@ -1,35 +1,35 @@
 // Import the Express library to help build the web server.
-var express = require('express');
+var express = require("express");
 
 // Create an instance of an Express application.
 var server = express();
 
 // Import routes from the './routes/routes' module.
-var routes = require('./routes/routes');
+var routes = require("./routes/routes");
 
 // Import the mongoose library to interact with MongoDB.
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 // Import the cors middleware to enable Cross-Origin Resource Sharing.
-const cors = require('cors');
+const cors = require("cors");
 
 // Set the 'strictQuery' option to false to allow flexibility in query conditions.
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
 // Connect to a MongoDB database with the provided URI, enabling new URL parser and unified topology.
 mongoose.connect(
-    "mongodb+srv://Joel:123@full-stack-web-applicat.fqa1a2h.mongodb.net/test&SolentNursery_childList",
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    function checkDB(error) {
-        // Callback function to handle the connection attempt.
-        if (error) {
-            // Log to the console if there is an error during the database connection.
-            console.log("error")
-        } else {
-            // Log to the console once successfully connected to the database.
-            console.log("Mongo DB (test&SolentNursery_childList) Connected!")
-        }
+  "mongodb+srv://Joel:123@full-stack-web-applicat.fqa1a2h.mongodb.net/test&SolentNursery_childList",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function checkDB(error) {
+    // Callback function to handle the connection attempt.
+    if (error) {
+      // Log to the console if there is an error during the database connection.
+      console.log("error");
+    } else {
+      // Log to the console once successfully connected to the database.
+      console.log("Mongo DB (test&SolentNursery_childList) Connected!");
     }
+  }
 );
 
 // Use CORS middleware to handle cross-origin requests.
@@ -43,11 +43,11 @@ server.use(routes);
 
 // Listen on port 8000 and handle the server start event.
 server.listen(8000, function check(error) {
-    if (error) {
-        // Log to the console if there's an error when starting the server.
-        console.log("error")
-    } else {
-        // Confirm in the console that the server is running and on which port.
-        console.log("Server started on Port 8000 (http://localhost:8000)")
-    }
+  if (error) {
+    // Log to the console if there's an error when starting the server.
+    console.log("error");
+  } else {
+    // Confirm in the console that the server is running and on which port.
+    console.log("Server started on Port 8000 (http://localhost:8000)");
+  }
 });
