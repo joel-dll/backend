d36a88cc7621995f0a781d7c5c8a7c163c512cd2
claudiamendes <1ribev81@solent.ac.uk>
2024-05-15 14:02:38 +0100

    usercontroller/model updated

---
+++ src/users/usersController.js
@@ -0,0 +1,33 @@
+const User = require('/usersModel');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.signup = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const user = await User.create({ username, email, password });
+    res.status(201).send('User created successfully');
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).send('Invalid credentials');
+    }
+
+    const token = jwt.sign({ id: user._id }, 'your-secret-key', { expiresIn: '1h' });
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
